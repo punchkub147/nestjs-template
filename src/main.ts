9e4ac2981cadaaccc67f70f3f84c0be4f1f9a724
kittapard <kittapard@perceptra.tech>
2022-07-18 22:50:43 +0700

    [Feature] Validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
@@ -7,6 +8,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      disableErrorMessages: false,
+    }),
+  );
 
   setupSwagger(app);
 
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty()
+  @IsString()
   firstName: string;
 
   @ApiProperty()
+  @IsString()
   lastName: string;
 
   @ApiProperty()
+  @IsBoolean()
   isActive: boolean;
 }
