50840cd9f2c80b91e6b4a88326c7fa78b73b77e1
kittapard <kittapard@perceptra.tech>
2022-07-18 22:04:20 +0700

    [Feature] Basic CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+
+const dbconfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'nestjs-template',
+  entities: [__dirname + '**/**/*.entity{.ts,.js}'],
+  synchronize: true,
+};
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(dbconfig), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
