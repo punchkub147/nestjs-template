137ae063fab0002ecbba90bef16f92e69d500c6d
kittapard <kittapard@perceptra.tech>
2022-07-18 22:31:53 +0700

    [Feature] init swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
+import { setupSwagger } from './utils/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+
+  setupSwagger(app);
 
   await app.listen(3000);
 }
---
+++ src/utils/swagger.ts
@@ -0,0 +1,12 @@
+import type { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication): void {
+  const options = new DocumentBuilder()
+    .setTitle('NestJS Documentation')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('documentation', app, document);
+}
