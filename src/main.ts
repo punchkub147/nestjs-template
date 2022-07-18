b424e8214b0557959a0984f323547869191aad72
kittapard <kittapard@perceptra.tech>
2022-07-19 00:01:09 +0700

    [Feature] enable cors

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,8 @@ import { setupSwagger } from './utils/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(
